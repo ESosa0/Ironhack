873dcce25c7b2250dfafe44a6795da7a2fbbc13d
ESosa0 <ericasosa0@gmail.com>
2016-01-29 14:50:16 +0100

    added more questions and ending to quiz

--- week_3/quiz_game/quiz.js
+++ week_3/quiz_game/quiz.js
@@ -7,10 +7,10 @@ var Question = function(question, answer, id) {
 };
 
 
-var Quiz = function() {
+var Quiz = function(quizName) {
   var allQuestions = [];
 
-  console.log("Quiz Time!");
+  console.log(quizName);
 
   this.askQuestion = function myself (index) {
     var index = index || 0
@@ -22,7 +22,12 @@ var Quiz = function() {
     function displayAnswer(err, answer){
       if (answer.toLowerCase() === allQuestions[index].answer){
         console.log("Correct!")
-        return myself(index+1);
+        if (index===allQuestions.length-1){
+          console.log("You've finished the quiz!")
+          return
+        } else {
+          return myself(index+1);
+        }
       } else {
         console.log("Incorrect")
         return myself(index);
@@ -39,9 +44,14 @@ var Quiz = function() {
 
 var question1 = new Question("What is the capital of Italy?", "rome", 1)
 var question2 = new Question("What is the capital of Spain?", "madrid", 2)
+var question3 = new Question("What is the capital of Ukraine?", "kiev", 3)
+var question4 = new Question("What is the capital of The Netherlands?", "amsterdam", 4)
 
-var aQuiz = new Quiz();
-aQuiz.getQuestions(question1);
-aQuiz.getQuestions(question2);
 
-aQuiz.askQuestion();
+var euroQuiz = new Quiz("Quiz time! Capitals of Europe!");
+euroQuiz.getQuestions(question1);
+euroQuiz.getQuestions(question2);
+euroQuiz.getQuestions(question3);
+euroQuiz.getQuestions(question4);
+
+euroQuiz.askQuestion();
